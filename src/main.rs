67112b95c993a03372dcd54737af77e2f3571e8e
Kevin Lai <zlnh4@umsystem.edu>
2021-06-30 15:03:00 -0500

    dtor termios

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello,   world!");
+use std::io;
+use termios::{ self };
+use termios::os::target::{ TCSANOW };
+
+struct Termios {
+    termios: termios::Termios,
 }
+
+impl Termios {
+    fn new() -> Termios
+    {
+        let cur_term = Termios { termios: termios::Termios::from_fd(0).unwrap() };
+        let new_term = termios::Termios::from_fd(0).unwrap();
+        termios::tcsetattr(0, TCSANOW, &new_term).unwrap();
+
+        println!("Set new termios");
+        return cur_term
+    }
+}
+
+impl Drop for Termios {
+    fn drop(&mut self)
+    {
+        println!("Restoring termios");
+        termios::tcsetattr(0, TCSANOW, &self.termios).unwrap();
+    }
+}
+
+
+
+fn main() {
+    let _ = Termios::new();   
+}
+
